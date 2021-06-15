e482bf0e2b9efeec2e67e3fff392434af2b82c0b
raduansm <75932979+raduansm@users.noreply.github.com>
2021-06-15 17:47:55 +0600

    BOOKINGS

--- lib/container/bookedTile.dart
+++ lib/container/bookedTile.dart
@@ -15,7 +15,7 @@ class BookedTile extends StatelessWidget {
       child: Container(
         color: Color(0xffefefef),
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.fromLTRB(15, 5, 15, 5),
           child: Column(
             children: [
               Row(
@@ -25,19 +25,25 @@ class BookedTile extends StatelessWidget {
                     children: [
                       Text(
                         bookDetails.userName != null ? bookDetails.userName : 'Mr xyz',
-                        style: GoogleFonts.roboto(fontSize: 14, height: 2, fontWeight: FontWeight.bold),
+                        style: GoogleFonts.roboto(fontSize: 16, height: 2, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        width: 5,
                       ),
                       Text(
-                        bookDetails.userPhoneNumber != null ? bookDetails.userPhoneNumber : '124587569854',
+                        bookDetails.userPhoneNumber != null ? "(${bookDetails.userPhoneNumber})" : '124587569854',
                         style: GoogleFonts.roboto(fontSize: 10, height: 2, color: Color(0xff6b6b6b)),
                       ),
                     ],
                   ),
                   Text(
-                    bookDetails.totalDiscountedPrice != null ? bookDetails.totalDiscountedPrice.toString() : '0',
+                    bookDetails.totalDiscountedPrice != null ? "৳ ${bookDetails.totalDiscountedPrice.toString()}" : '0',
                     style: GoogleFonts.roboto(fontSize: 16, height: 2, fontWeight: FontWeight.bold),
                   ),
                 ],
+              ),
+              SizedBox(
+                height: 10,
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -45,29 +51,61 @@ class BookedTile extends StatelessWidget {
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
-                        'ST021,STO13',
-                        style: GoogleFonts.roboto(fontSize: 10, height: 2, color: Color(0xff6b6b6b)),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Image.asset(
+                            'images/key.png',
+                            scale: 13,
+                            fit: BoxFit.fill,
+                          ),
+                          SizedBox(
+                            width: 12,
+                          ),
+                          Text(
+                            'ST021,STO13',
+                            style: GoogleFonts.roboto(fontSize: 10, height: 1, color: Color(0xff6b6b6b)),
+                          ),
+                        ],
                       ),
-                      Text(
-                        '${DateFormat('dd-MM-yy').format(DateTime.parse(bookDetails.dateRange["startDate"].toDate().toString()))}  -  ${DateFormat('dd-MM-yy').format(DateTime.parse(bookDetails.dateRange["endDate"].toDate().toString()))}',
-                        // '${DateTime.parse(bookDetails.dateRange["startDate"].toDate().toString())}',
-                        style: GoogleFonts.roboto(fontSize: 10, height: 2, color: Color(0xff6b6b6b)),
-                      )
+                      SizedBox(
+                        height: 3,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Image.asset(
+                            'images/calendar.png',
+                            scale: 12,
+                            fit: BoxFit.fill,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            '${DateFormat('dd-MM-yy').format(DateTime.parse(bookDetails.dateRange["startDate"].toDate().toString()))}  -  ${DateFormat('dd-MM-yy').format(DateTime.parse(bookDetails.dateRange["endDate"].toDate().toString()))}',
+                            // '${DateTime.parse(bookDetails.dateRange["startDate"].toDate().toString())}',
+                            style: GoogleFonts.roboto(fontSize: 8, height: 1.5, color: Color(0xff6b6b6b)),
+                          ),
+                        ],
+                      ),
                     ],
                   ),
                   GestureDetector(
                     onTap: null,
-                    child: Container(
-                      color: Colors.black,
-                      child: Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: Text(
-                          'CHECK IN',
-                          style: GoogleFonts.roboto(
-                            fontSize: 12,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
+                      child: Container(
+                        color: Colors.black,
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(15, 4, 15, 4),
+                          child: Text(
+                            'CHECK IN',
+                            style: GoogleFonts.roboto(
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
                           ),
                         ),
                       ),
--- lib/container/paddingTile.dart
+++ lib/container/paddingTile.dart
@@ -10,58 +10,81 @@ class PendingTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
-          color: Color(0xffefefef),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          bookDetails.userName != null ? bookDetails.userName : 'Mr xyz',
-                          style: GoogleFonts.roboto(fontSize: 14, height: 2, fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          bookDetails.userPhoneNumber != null ? bookDetails.userPhoneNumber : '124587569854',
-                          style: GoogleFonts.roboto(fontSize: 10, height: 2, color: Color(0xff6b6b6b)),
-                        ),
-                      ],
-                    ),
-                    Text(
-                      bookDetails.totalDiscountedPrice != null ? bookDetails.totalDiscountedPrice.toString() : '0',
-                      style: GoogleFonts.roboto(fontSize: 16, height: 2, fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'ST021,STO13',
-                          style: GoogleFonts.roboto(fontSize: 10, height: 2, color: Color(0xff6b6b6b)),
-                        ),
-                        Text(
-                          '${DateFormat('dd-MM-yy').format(DateTime.parse(bookDetails.dateRange["startDate"].toDate().toString()))}  -  ${DateFormat('dd-MM-yy').format(DateTime.parse(bookDetails.dateRange["endDate"].toDate().toString()))}',
-                          style: GoogleFonts.roboto(fontSize: 10, height: 2, color: Color(0xff6b6b6b)),
-                        )
-                      ],
-                    ),
-                    GestureDetector(
-                      onTap: null,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        color: Color(0xffefefef),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(15, 5, 15, 5),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    bookDetails.userName != null ? bookDetails.userName : 'Mr xyz',
+                    style: GoogleFonts.roboto(fontSize: 16, height: 2, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    bookDetails.totalDiscountedPrice != null ? "৳ ${bookDetails.totalDiscountedPrice.toString()}" : '৳ 0',
+                    style: GoogleFonts.roboto(fontSize: 16, height: 1, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Image.asset(
+                            'images/calendar.png',
+                            scale: 11,
+                            fit: BoxFit.fill,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            '${DateFormat('dd-MM-yy').format(DateTime.parse(bookDetails.dateRange["startDate"].toDate().toString()))}  -  ${DateFormat('dd-MM-yy').format(DateTime.parse(bookDetails.dateRange["endDate"].toDate().toString()))}',
+                            style: GoogleFonts.roboto(fontSize: 12, height: 1, color: Color(0xff6b6b6b)),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        children: [
+                          Image.asset(
+                            'images/key.png',
+                            scale: 10,
+                            fit: BoxFit.fill,
+                          ),
+                          SizedBox(
+                            width: 12,
+                          ),
+                          Text(
+                            '2 ROOMS',
+                            style: GoogleFonts.roboto(fontSize: 8, height: 2, color: Color(0xff6b6b6b)),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  GestureDetector(
+                    onTap: null,
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
                       child: Container(
                         color: Colors.black,
                         child: Padding(
-                          padding: const EdgeInsets.all(4.0),
+                          padding: const EdgeInsets.fromLTRB(15, 4, 15, 4),
                           child: Text(
                             'ACCEPT',
                             style: GoogleFonts.roboto(
@@ -73,10 +96,10 @@ class PendingTile extends StatelessWidget {
                         ),
                       ),
                     ),
-                  ],
-                ),
-              ],
-            ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
